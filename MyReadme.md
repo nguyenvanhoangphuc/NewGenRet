7f55a2100dc5ffea07eb17f8195d58158765131d
toilaphucnguyen <phucknguyen123456789@gmail.com>
2024-08-12 13:19:32 +0000

    update MyReadme.md

--- MyReadme.md
+++ MyReadme.md
@@ -7,15 +7,16 @@ conda deactivate
 # cài đặt pytorch
 conda install pytorch torchvision torchaudio pytorch-cuda=11.8 -c pytorch -c nvidia
 
-# unzip in window
+# unzip in window (không cần thiết nếu không train toàn bộ nq320k)
 tar -xf dataset/nq320k.zip
 
-# run genret for nq320k dataset
-python run.py --model_name t5-base --code_num 512 --max_length 3 --train_data nq320k/train.json --dev_data nq320k/dev.json --corpus_data nq320k/corpus_lite.json --save_path out/model
+# run train for nq320k split using bm25 dataset (copy dòng lệnh trên bỏ qua vị trí run train trong run_train.sh)
+# ban đầu
+python run.py --model_name t5-base --code_num 512 --max_length 3 --train_data dataset/ms_marco_bm25_1208/train_retrieval_ms_marco.json --dev_data dataset/ms_marco_bm25_1208/validation_retrieval_ms_marco.json --corpus_data dataset/ms_marco_bm25_1208/legal_corpus_ms_marco.json --save_path out_bm25/model >> output.log
+# Phuc tách thành các file chuyên biệt
+python my_run.py --model_name t5-base --code_num 512 --max_length 3 --train_data dataset/ms_marco_bm25_1208/train_retrieval_ms_marco.json --dev_data dataset/ms_marco_bm25_1208/validation_retrieval_ms_marco.json --corpus_data dataset/ms_marco_bm25_1208/legal_corpus_ms_marco.json --save_path out_bm25/model >> output.log
 
-python run.py --model_name t5-base --code_num 512 --max_length 3 --train_data nq320k_split/train.json --dev_data nq320k_split/dev.json --corpus_data nq320k_split/corpus_lite.json --save_path out/model
-
-Chú thích: 
+# Về format của dataset nq320k: (code repo này đã thay đổi format đầu vào nên chạy tập này sẽ lỗi)
 - file train.json là một list các item, mỗi item là một list gồm 2 phần tử, phần tử đầu tiên sẽ là question (hoặc query), phần tử thứ 2 là số thứ tự (id) của document mà nó liên quan đến trong bộ corpus_lite.json
 + số lượng item: 307373
 - file corpus_lite.json là một list các document, mỗi document là một chuỗi string, thứ tự của document cũng chính là id của document đó.
@@ -24,3 +25,16 @@ Chú thích:
 + số lượng item: 1097390
 - file dev.json tương tự như train.json nhưng số lượng ít hơn
 + số lượng item: 7830
+
+# Format dataset chạy được 
+- phải thuộc dạng dataset legal corpus gồm 4 file train, valid, test, legal_corpus.
+
+# Tinh chỉnh epochs huấn luyện
+lần lượt tạo ra các thư mục model-1-pre, model-1, model-2-pre, model-2, model-3-pre, model-3, model-3-fit
+số epochs huấn luyện có thể được quy định, mặc định thì model-1-pre là 1 epoch, model-2-pre, model-3-pre là 10 epochs, 
+model-1, model-2, model-3 mặc định là 200 mà vì mình lưu step là 9 nên phải là bội của 9 + 1, hiện tại trong code là 199 (dòng 658 file my_run.py)
+model-3-fit mặc định là 1000 thoã mãn bội của 9 + 1 mà nó hơi nhiều nên giảm xuống hiện tại trong code là 541 (dòng 671 file my_run.py)
+
+
+# Chỉnh batch_size:
+- Tìm kiếm in_batch_size dòng 635, chỉnh từ 32 lên 128 nếu như có GPU dư
--- run.py
+++ run.py
@@ -1218,47 +1218,45 @@ def main():
     args = parse_args()
     # tạo config là một bản sao của args
     config = copy.deepcopy(vars(args))
-    # # khởi tạo checkpoint ban đầu là None
-    # checkpoint = "out_msmarco/model-2/198.pt"
-    # # lặp qua các giá trị từ 0 đến max_length-1 (3-1)
-    # for loop in range(2, args.max_length):
-    #     # cập nhật save_path, code_length theo loop
-    #     config['save_path'] = args.save_path + f'-{loop + 1}-pre'
-    #     config['code_length'] = loop + 1
-    #     # prev_model là checkpoint trước đó, đi kèm prev_id là file code của checkpoint trước đó (file này sẽ được tạo ra sau khi chạy train)
-    #     config['prev_model'] = checkpoint
-    #     config['prev_id'] = f'{checkpoint}.code' if checkpoint is not None else None
-    #     # lần đầu tiên chỉ chạy 1 epoch, sau đó chạy 10 epoch
-    #     config['epochs'] = 1 if loop == 0 else 10
-    #     config['loss_w'] = 1 
-    #     # chạy train
-    #     checkpoint = train(config)
-    #     test_dr(config)
+    # khởi tạo checkpoint ban đầu là None
+    checkpoint = None
+    # lặp qua các giá trị từ 0 đến max_length-1 (3-1)
+    for loop in range(args.max_length):
+        # cập nhật save_path, code_length theo loop
+        config['save_path'] = args.save_path + f'-{loop + 1}-pre'
+        config['code_length'] = loop + 1
+        # prev_model là checkpoint trước đó, đi kèm prev_id là file code của checkpoint trước đó (file này sẽ được tạo ra sau khi chạy train)
+        config['prev_model'] = checkpoint
+        config['prev_id'] = f'{checkpoint}.code' if checkpoint is not None else None
+        # lần đầu tiên chỉ chạy 1 epoch, sau đó chạy 10 epoch
+        config['epochs'] = 1 if loop == 0 else 10
+        config['loss_w'] = 1 
+        # chạy train
+        checkpoint = train(config)
+        test_dr(config)
 
-    #     config['save_path'] = args.save_path + f'-{loop + 1}'
-    #     config['prev_model'] = checkpoint
-    #     config['codebook_init'] = f'{checkpoint}.kmeans.{args.code_num}'
-    #     config['epochs'] = 199   # default: 200  #should 199
-    #     config['loss_w'] = 2
-    #     checkpoint = train(config)
-    #     test_dr(config)
+        config['save_path'] = args.save_path + f'-{loop + 1}'
+        config['prev_model'] = checkpoint
+        config['codebook_init'] = f'{checkpoint}.kmeans.{args.code_num}'
+        config['epochs'] = 199   # default: 200  #should 199
+        config['loss_w'] = 2
+        checkpoint = train(config)
+        test_dr(config)
 
-    #     test(config)
-    checkpoint = "out_msmarco/model-3/198.pt"
+        test(config)
+
     loop = args.max_length
     config['save_path'] = args.save_path + f'-{loop}-fit'
     config['code_length'] = loop + 1
     config['prev_model'] = checkpoint
-    # add_last(f'{checkpoint}.code', args.code_num, f'{checkpoint}.code.last')
+    add_last(f'{checkpoint}.code', args.code_num, f'{checkpoint}.code.last')
     config['prev_id'] = f'{checkpoint}.code'
     config['epochs'] = 541     # default: 1000 # should: 1000
     config['loss_w'] = 3
-    # checkpoint = train(config)
-    checkpoint = "out_msmarco/model-3-fit/540.pt"
-    # test_dr(config)
+    checkpoint = train(config)
+    test_dr(config)
     test(config)
     print(checkpoint)
-
 
 if __name__ == '__main__':
     main()
